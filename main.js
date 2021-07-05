8baf20c76506b9f952475245deb6224ebf0ba7e1
Mahmoud Idris <mahmouddrs@gmail.com>
2021-07-05 08:42:27 +0000

    making progress

--- main.js
+++ main.js
@@ -1,44 +1,13 @@
-const list = document.querySelector('.list');
-const addBtn = document.querySelector('#addbtn');
-//const inputvalue = ;
+const inputArea = document.getElementById('input');
+let lists = true;
+document.getElementById('sidemenuBtn').onclick = ()=>{
+    document.querySelector('.sidemenu').classList.toggle("open");
+    console.log("Added");
+    inputArea.placeholder = lists ? "Add New List" : "Add New Task";
+    lists == true ? lists = false : lists = true;
 
-const LOCAL_STORAGE_KEY = "KEY.KEY"
-let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || []
 
-function render(){
-    list.innerHTML = ""
-lists.forEach(element => {
-const item = document.createElement('li');
-item.classList.add('item');
-item.innerText = element;
-list.appendChild(item)})
 }
 
-
-
- 
-addBtn.addEventListener('click', ()=>{
-
-    let input = document.querySelector('#input').value;
-    if (input == null || input == "") {return};
-    lists.push(input);
-    
-    render()
-    save()
-})
-
-function deletelocalStorageList (){ localStorage.removeItem(LOCAL_STORAGE_KEY);}
-
-    
-
-function save (){
-    console.log(lists)
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(lists))
-}
-
-document.getElementById('delete').addEventListener('click', ()=>{
-    console.log('DELETED!')
-    deletelocalStorageList()
-})
 
 
