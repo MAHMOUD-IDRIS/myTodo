2fb7d42ade75458ff9e718ba3c0f069af2a81a93
Mahmoud Idris <mahmouddrs@gmail.com>
2021-07-06 06:08:12 +0000

    working on list buttons

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
 const inputArea = document.getElementById('input');
-let lists = true;
+let sideMenuOpen = true;
 document.getElementById('sidemenuBtn').onclick = ()=>{
     document.querySelector('.sidemenu').classList.toggle("open");
     console.log("Added");
-    inputArea.placeholder = lists ? "Add New List" : "Add New Task";
-    lists == true ? lists = false : lists = true;
+    inputArea.placeholder = sideMenuOpen ? "Add New List" : "Add New Task";
+    sideMenuOpen == true ? sideMenuOpen = false : sideMenuOpen = true;
     document.querySelector(".sidemenu-btn").classList.toggle('open');
 
 
 }
 
-//getting list elements and working on them
-const listElements = document.querySelectorAll('li');
+const data = {
+    lists: [{id: 1, done: true , list: "groceries", tasks: [{id: 1.1 ,done: true , task: "buy tomato"}]},
+    {id: 2, done: true , list: "school", tasks: [{id: 2.1 ,done: true , task: "class at 5"}]}
+], 
+}
 
-listElements.forEach(item => {
-    item.onclick = done();
-})
+for (let element in data.lists){
+    console.log(data.lists[element].list)
+}
 
-function done (){
-console.log('clicked for done!')
+for (let element in data.lists){
+    console.log(data.lists[element].tasks[0].task)
 }
 
 
